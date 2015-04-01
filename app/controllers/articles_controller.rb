fda3ae7780dd0fbc740e2734bacab6dc0bb90dcd
Jason Ding (a501832) <jason.ding@fmr.com>
2015-04-01 16:09:47 -0400

    add comments and authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	http_basic_authenticate_with name: "rails", password: "blog", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
